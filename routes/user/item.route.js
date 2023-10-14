2fd26b853e5dd10eac0d15eab32e82909a670bcd
youssefkatry907 <youssefkatry907@gmail.com>
2023-10-15 00:08:15 +0300

    Last update

--- routes/user/item.route.js
+++ routes/user/item.route.js
@@ -1,7 +1,7 @@
 const app = require("express").Router();
 const itemController = require("../../controllers/user/item.controller");
 const validator = require("../../helpers/validation.helper");
-const { addItemValidation } = require("../../validation/Item/addItemValidation.js");
+const addItemValidation = require("../../validation/Item/addItemValidation.js");
 
 app.post("/add", validator(addItemValidation), itemController.add);
 app.get("/list", itemController.list);

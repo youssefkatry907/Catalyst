abaaed8f81a2c11c7aa596701fd9bad3551eaa52
youssefkatry907 <youssefkatry907@gmail.com>
2023-10-17 01:22:25 +0300

    change port

--- config/server.js
+++ config/server.js
@@ -4,8 +4,8 @@ const http = require('http');
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+server.listen(8000, () => {
+  console.log("Server is listening on port 8000");
 });
 
 module.exports = app;

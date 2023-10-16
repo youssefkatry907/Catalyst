b69e966625ce45b46e7f2988eebce7eb950e213d
youssefkatry907 <youssefkatry907@gmail.com>
2023-10-17 01:13:04 +0300

    createItemValidation

--- routes/user/item.route.js
+++ routes/user/item.route.js
@@ -3,6 +3,8 @@ const itemController = require("../../controllers/user/item.controller");
 let validator = require("../../helpers/validation.helper");
 let { addItemValidation } = require("../../validation/Item/createItemValidation");
 
+// new commit
+
 app.post("/add", validator(addItemValidation), itemController.add);
 app.get("/list", itemController.list);
 app.get("/get", itemController.get);

5436136e7c281187e9aeaf3beab5e0881cb98ef7
youssefkatry907 <youssefkatry907@gmail.com>
2023-10-17 18:26:56 +0300

    Port config

--- config/app.js
+++ config/app.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 app.use(express.json());
-app.use(cors());
 require("dotenv").config();
 const databaseConnection = require("./database").connection;
 
 const routes = require("../routes/index.route");
 
-
-
-//const cors = require("cors");
-
-
 databaseConnection();
-
-
-//app.use(cors());
 
 app.use(routes);
 
--- config/server.js
+++ config/server.js
@@ -1,18 +1,10 @@
 const app = require("./app");
 const http = require('http');
 
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Authorization', 'Content-Type', "Accept-Language"]
-};
-
-
-
 const server = http.createServer(app);
 
 server.listen(process.env.PORT || 8000, () => {
-  console.log("Server is listening on port 8000");
+  console.log(`Server is listening on port ${process.env.PORT}"`);
 });
 
 module.exports = app;

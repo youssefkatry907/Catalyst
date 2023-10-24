a95a5867766d0783fd55fbb211bb14145c5def06
youssefkatry907 <youssefkatry907@gmail.com>
2023-10-24 14:44:04 +0300

    AuthHeaders

--- helpers/jwt.helper.js
+++ helpers/jwt.helper.js
@@ -5,20 +5,24 @@ exports.generateToken = (payload, expiresIn) => {
     expiresIn = expiresIn ? expiresIn : "365d"
     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET || "secret", { expiresIn })
 }
-
+// console.log(`process.env.ACCESS_TOKEN_SECRET`, process.env.ACCESS_TOKEN_SECRET)
 exports.verifyToken = (role) => {
     return (req, res, next) => {
         let authHeader = req.headers['authorization']
-        const token = authHeader && authHeader.split(" ")[1]
-        if (token) {
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET || "secret", (err, tokenData) => {
-                if (err) return res.status(403).json({ success: false, message: "Invalid token", code: 403 })
-                if (!role.includes(tokenData.role)) return res.status(401).json({ success: false, message: "Unauthorized user", code: 401 })
-                req.tokenData = tokenData;
-                next();
-            })
+        if (authHeader) {
+            let token = authHeader && authHeader.split(" ")[1]
+            if (token) {
+                jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, tokenData) => {
+                    if (err) return res.status(403).json({ success: false, message: "Invalid token", code: 403 })
+                    if (!role.includes(tokenData.role)) return res.status(401).json({ success: false, message: "Unauthorized user", code: 401 })
+                    req.tokenData = tokenData;
+                    next();
+                })
 
-        } else return res.status(401).json({ success: false, message: "unauthorized", code: 401 })
+            } else return res.status(401).json({ success: false, message: "unauthorized", code: 401 })
+        } else {
+            return res.status(401).json({ success: false, message: "unauthorized", code: 401 })
+        }
     }
 
 }

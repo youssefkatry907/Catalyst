8f34665f1e43a09d87de51d33188a8df34ad1d97
youssefkatry907 <youssefkatry907@gmail.com>
2023-11-20 18:33:53 +0200

    Allow admin acess the app

--- routes/user/index.route.js
+++ routes/user/index.route.js
@@ -3,7 +3,6 @@ const app = express();
 
 let checkToken = require("../../helpers/jwt.helper").verifyToken;
 const allowedUsers = ["user", "admin"]
-// const allowedAdmins = ["admin"]
 
 const authRoutes = require("./auth.route");
 const userRoutes = require("./user.route");
@@ -14,12 +13,10 @@ const catalogRoutes = require("./catalog.route");
 const metalRoutes = require("./metal.route");
 const inboxRoutes = require("./inbox.route");
 
-console.log("Users: ",allowedUsers[0]);
 
 app.use(authRoutes);
 app.use(checkToken(allowedUsers), userRoutes);
 app.use("/item", checkToken(allowedUsers), itemRoutes);
- //app.use("/item", checkToken(allowedAdmins), itemRoutes);
 app.use("/list", checkToken(allowedUsers), listRoutes);
 app.use("/favorite", checkToken(allowedUsers), favoriteRoutes);
 app.use("/catalog", checkToken(allowedUsers), catalogRoutes);

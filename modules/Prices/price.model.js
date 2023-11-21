c225bbea173f48c8bc4426685d68b44eef1e59d6
youssefkatry907 <youssefkatry907@gmail.com>
2023-11-21 16:58:06 +0200

    Prices Module

--- modules/Prices/price.model.js
+++ modules/Prices/price.model.js
@@ -2,8 +2,8 @@ let mongoose = require('mongoose');
 
 let priceSchema = mongoose.Schema({
     subscriptionType: { type: String, required: true },
-    month: { type: Object },
-    year: { type: Object },
+    month: { type: Object, required: true },
+    year: { type: Object, required: true },
     totalMonthlyPrice: { type: Number, required: true },
     totalYearlyPrice: { type: Number, required: true },
 })

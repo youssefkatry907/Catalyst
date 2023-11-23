03d2884adb8eb57df5b7851a9c489af1d577e644
youssefkatry907 <youssefkatry907@gmail.com>
2023-11-23 18:11:57 +0200

    Inbox

--- modules/Inbox/inbox.repo.js
+++ modules/Inbox/inbox.repo.js
@@ -42,11 +42,6 @@ exports.addNote = async (catalogId, form) => {
 exports.listNotes = async (userId) => {
     try {
         let inbox = await Inbox.findOne({ userId });
-        if (!inbox) return {
-            success: false,
-            code: 404,
-            message: "User not found"
-        }
         return {
             success: true,
             code: 200,

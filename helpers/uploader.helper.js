fae8fd65a3cd4aa95abad0babe681bbc16f56ecb
youssefkatry907 <youssefkatry907@gmail.com>
2023-12-16 21:08:32 +0200

    Upload userImage

--- helpers/uploader.helper.js
+++ helpers/uploader.helper.js
@@ -9,37 +9,8 @@ function fileFilter(req, file, cb) {
     };
 }
 
-const storage = multer.diskStorage({});
+const storage = multer.memoryStorage({});
 
 const uploadImage = multer({ storage, fileFilter });
 
 module.exports = uploadImage;
-
-// exports.uploadImage = (folderName) => {
-//   const storage = multer.diskStorage({});
-
-//   return multer({ storage: storage });
-
-// }
-
-// exports.handleFileUploadErrors = (error, req, res, next) => {
-//   if (error instanceof multer.MulterError) {
-//     if (error.code === "LIMIT_FILE_SIZE") {
-//       return res.status(400).json({
-//         message: "file is too large",
-//       });
-//     }
-
-//     if (error.code === "LIMIT_FILE_COUNT") {
-//       return res.status(400).json({
-//         message: "File limit reached",
-//       });
-//     }
-
-//     if (error.code === "LIMIT_UNEXPECTED_FILE") {
-//       return res.status(400).json({
-//         message: "File must be an image",
-//       });
-//     }
-//   }
-// }
--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -1,75 +1,94 @@
-// console.log(`process.env.CLOUD_NAME`, process.env.CLOUD_NAME)
+// let cloudinary = require('../config/cloud');
+// const axios = require('axios');
+// const { Buffer } = require('buffer');
 
-// exports.uploadImage = async (image) => {
+// exports.uploadImageToCloudinary = async (file, publicId, path) => {
 //     try {
-//         const uploadedImage = await cloudinary.uploader.upload(image, { folder: "items" });
-//         console.log(`uploadedImage`, uploadedImage)
+//         const result = await cloudinary.v2.uploader.upload(file, {
+//             folder: path,
+//             public_id: publicId,
+//             overwrite: true,
+//         },
+//             function (error, result) {
+//                 if (error) {
+//                     return {
+//                         success: false,
+//                         statusCode: 500,
+//                         message: "something went wrong !"
+//                     }
+//                 };
+//             });
 //         return {
 //             success: true,
-//             code: 200,
-//             url: uploadedImage.secure_url
+//             statusCode: 201,
+//             message: "success",
+//             url: result.url,
+//             public_id: result.public_id
 //         }
-//     } catch (err) {
-//         console.log(`err.message`, err.message);
+//     }
+//     catch (err) {
 //         return {
 //             success: false,
-//             code: 500,
+//             statusCode: 500,
 //             message: err.message
-//         };
+//         }
 //     }
 // }
 
-let cloudinary = require('../config/cloud');
-const axios = require('axios');
-const { Buffer } = require('buffer');
+// exports.urlToUnit8Array = async (url) => {
+//     try {
+//         const response = await axios.get(url, { responseType: 'arraybuffer' });
+//         const uint8Array = new Uint8Array(response.data);
+//         const buffer = Buffer.from(uint8Array);
+//         //console.log(`buffer`, buffer)
+//         return buffer;
+//     }
+//     catch (err) {
+//         return {
+//             success: false,
+//             statusCode: 500,
+//             message: err.message
+//         }
+//     }
+// }
 
-exports.uploadImageToCloudinary = async (file, publicId, path) => {
+
+let AWS = require('aws-sdk');
+const uuid = require("uuid").v4
+
+require("dotenv").config();
+const s3 = new AWS.S3({
+    endpoint: process.env.BUCKET_ENDPOINT,
+    accessKeyId: process.env.BUCKET_ACCESS_KEY,
+    secretAccessKey: process.env.BUCKET_SECRET_KEY,
+    s3BucketEndpoint: true,
+});
+
+
+
+exports.uploadFileToS3 = async (folderName, file) => {
     try {
-        const result = await cloudinary.v2.uploader.upload(file, {
-            folder: path,
-            public_id: publicId,
-            overwrite: true,
-        },
-            function (error, result) {
-                if (error) {
-                    return {
-                        success: false,
-                        statusCode: 500,
-                        message: "something went wrong !"
-                    }
-                };
-            });
-        return {
-            success: true,
-            statusCode: 201,
-            message: "success",
-            url: result.url,
-            public_id: result.public_id
-        }
-    }
-    catch (err) {
-        return {
-            success: false,
-            statusCode: 500,
-            message: err.message
-        }
+        const params = {
+            Bucket: process.env.BUCKET_NAME,
+            Key: `public/${folderName}/${uuid()}-${file.originalname}`,
+            Body: file.buffer
+        };
+        return await s3.upload(params).promise();
+    } catch (err) {
+        console.log(`err.message`, err.message);
+        return err.message
     }
 }
 
-exports.urlToUnit8Array = async (url) => {
-    try {
-        const response = await axios.get(url, { responseType: 'arraybuffer' });
-        const uint8Array = new Uint8Array(response.data);
-        const buffer = Buffer.from(uint8Array);
-        //console.log(`buffer`, buffer)
-        return buffer;
-    }
-    catch (err) {
-        return {
-            success: false,
-            statusCode: 500,
-            message: err.message
-        }
-    }
-}
-
+exports.uploadPDFToS3 = (folderName) => {
+    // storage = multer.diskStorage({
+    //     destination: function (req, file, cb) {
+    //         cb(null, `uploads/${folderName}`)
+    //     },
+    //     filename: function (req, file, cb) {
+    //         cb(null, Date.now()+ Math.random() + '.pdf') //Appending .jpg
+    //     }
+    // })
+    const storage = multer.memoryStorage()
+    return multer({ storage: storage })
+  }
